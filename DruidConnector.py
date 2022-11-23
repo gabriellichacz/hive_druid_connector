8386881ef22d375f4e3acd7b47f044e768ccf28f
gabriellichacz <gabriellichacz@gmail.com>
2022-11-23 22:48:30 +0100

    Druid connector

--- DruidConnector.py
+++ DruidConnector.py
@@ -1,18 +1,46 @@
-import json
-import requests
+# python 3.8.10 64-bit
+def druidConnect(param_host, param_port = "8082") -> 'string':
+  """
+  Connect to Druid database
 
-url = "http://192.168.1.112:8082/druid/v2/sql/task/" # broker port musi być
+	Parameters:
+  param_host (string): Druid database host
+  param_port (string): Druid broker port | default = "8082"
 
-payload = json.dumps({
-  "query": "INSERT INTO wikipedia\nSELECT\n  TIME_PARSE(\"timestamp\") AS __time,\n  *\nFROM TABLE(\n  EXTERN(\n    '{\"type\": \"http\", \"uris\": [\"https://druid.apache.org/data/wikipedia.json.gz\"]}',\n    '{\"type\": \"json\"}',\n    '[{\"name\": \"added\", \"type\": \"long\"}, {\"name\": \"channel\", \"type\": \"string\"}, {\"name\": \"cityName\", \"type\": \"string\"}, {\"name\": \"comment\", \"type\": \"string\"}, {\"name\": \"commentLength\", \"type\": \"long\"}, {\"name\": \"countryIsoCode\", \"type\": \"string\"}, {\"name\": \"countryName\", \"type\": \"string\"}, {\"name\": \"deleted\", \"type\": \"long\"}, {\"name\": \"delta\", \"type\": \"long\"}, {\"name\": \"deltaBucket\", \"type\": \"string\"}, {\"name\": \"diffUrl\", \"type\": \"string\"}, {\"name\": \"flags\", \"type\": \"string\"}, {\"name\": \"isAnonymous\", \"type\": \"string\"}, {\"name\": \"isMinor\", \"type\": \"string\"}, {\"name\": \"isNew\", \"type\": \"string\"}, {\"name\": \"isRobot\", \"type\": \"string\"}, {\"name\": \"isUnpatrolled\", \"type\": \"string\"}, {\"name\": \"metroCode\", \"type\": \"string\"}, {\"name\": \"namespace\", \"type\": \"string\"}, {\"name\": \"page\", \"type\": \"string\"}, {\"name\": \"regionIsoCode\", \"type\": \"string\"}, {\"name\": \"regionName\", \"type\": \"string\"}, {\"name\": \"timestamp\", \"type\": \"string\"}, {\"name\": \"user\", \"type\": \"string\"}]'\n  )\n)\nPARTITIONED BY DAY",
-  "context": {
-    "maxNumTasks": 3
-  }
-})
-headers = {
-  'Content-Type': 'application/json'
-}
+	Returns:
+  url (string): Druid connection url
+  """
+  try:
+    import json
+    import requests
+  except:
+    cur = 'Couldn\'t import json or requests library'
 
-response = requests.request("POST", url, headers=headers, data=payload)
+  try:
+    url = "http://" + param_host + ":" + param_port + "/druid/v2/sql/task/"
+  except:
+    url = 'Couldn\'t connect to Druid broker'
+      
+  return url
 
-print(response.text)
+def druidInsert(druidUrl, sqlQuery) -> 'string':
+  """
+  Create 'insert into' task
+
+  Parameters:
+  druidUrl (string): Druid database host URL
+  sqlQuery (string): SQL insert query
+
+  Returns:
+  response (string): Druid response
+  """
+  payload = json.dumps({
+      "query": sqlQuery,
+      "context": {
+          "maxNumTasks": 3
+      }
+  })
+  headers = {'Content-Type': 'application/json'}
+  response = requests.request("POST", druidUrl, headers=headers, data=payload)
+
+  return response
