57f18af66fdb229b5230546b8e6201bcd5a68d6a
gabriellichacz <gabriellichacz@gmail.com>
2022-11-26 15:44:15 +0100

    Count update

--- CountData.py
+++ CountData.py
@@ -9,10 +9,10 @@ hive = hiveConnect(hive_host, hive_port, hive_username, hive_pass)
 
 ########### Making sure base data warehouse tables in Hive are of correct size ###########
 
-airlinesLen = countData('airlines', 'code')
-airfieldsLen = countData('airfields', 'airport_id')
-statesLen = countData('states', 'dest_state')
-flightsLen = countData('flights', 'flight_id')
+airlinesLen = countData(hive, 'airlines', 'code')
+airfieldsLen = countData(hive, 'airfields', 'airport_id')
+statesLen = countData(hive, 'states', 'dest_state')
+flightsLen = countData(hive, 'flights', 'flight_id')
 
 print('airlines length:', airlinesLen)
 print('airfields length:', airfieldsLen)
