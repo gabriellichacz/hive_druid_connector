d2bb3017e31e7326fbe758512ffdf3a46f6c0be3
gabriellichacz <gabriellichacz@gmail.com>
2022-11-27 19:48:01 +0100

    Delete

--- HiveInvoke.py
+++
@@ -1,8 +0,0 @@
-from HiveConnector import *
-import time
-
-hive_host = '192.168.1.112'
-hive_port = '10000'
-hive_username = 'stud'
-hive_pass = 'stud'
-hive = hiveConnect(hive_host, hive_port, hive_username, hive_pass)
