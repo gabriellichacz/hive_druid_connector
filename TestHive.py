f2228913a6f7d0a58415d722e54b07498d039a51
gabriellichacz <gabriellichacz@gmail.com>
2022-12-22 22:32:35 +0100

    performance test disturber

---
+++ TestHive.py
@@ -0,0 +1,15 @@
+from HiveConnector import *
+import time
+
+hive_host = '192.168.1.112'
+hive_port = '10000'
+hive_username = 'stud'
+hive_pass = 'stud'
+hive = hiveConnect(hive_host, hive_port, hive_username, hive_pass)
+
+while True:
+    hiveSelect(hive, ['airline', 'dest'], 'druid_flights', limit=10)
+    hiveSelect(hive, ['tail_number', 'taxi_out', 'taxi_in', 'airline'], 'druid_flights', where_column='airline', where_sign='=', where='B6', limit=500)
+    hiveShowTablesList(hive)
+    hiveSelect(hive, ['tail_number', 'origin', 'dest', 'arr_time'], 'druid_flights', where_column='arr_time', where_sign='<', where='15', limit=100)
+    time.sleep(10)
