0365446250eb825e1fa54145b4c1ef07c6c22fcf
gabriellichacz <gabriellichacz@gmail.com>
2022-12-22 22:44:17 +0100

    more reasonable test

--- TestHive.py
+++ TestHive.py
@@ -12,4 +12,4 @@ while True:
     hiveSelect(hive, ['tail_number', 'taxi_out', 'taxi_in', 'airline'], 'druid_flights', where_column='airline', where_sign='=', where='B6', limit=500)
     hiveShowTablesList(hive)
     hiveSelect(hive, ['tail_number', 'origin', 'dest', 'arr_time'], 'druid_flights', where_column='arr_time', where_sign='<', where='15', limit=100)
-    time.sleep(10)
+    time.sleep(30)
