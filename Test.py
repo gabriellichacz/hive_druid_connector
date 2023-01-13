38fdb7477da077bdd775c29715bcc976fb181cc4
gabriellichacz <gabriellichacz@gmail.com>
2023-01-13 16:00:58 +0100

    more tests update

--- Test.py
+++ Test.py
@@ -8,7 +8,9 @@ kafkaIp = '192.168.1.112'
 csvName1 = 'data_5tys.csv'
 csvName3 = 'data_500tys.csv'
 csvName4 = 'data_1mln.csv'
-csvName5 = 'data_5mln.csv'
+csvName5 = 'data_2mln.csv'
+csvName6 = 'data_4mln.csv'
+csvName7 = 'data_5mln.csv'
 
 def performanceTest(druidSqlUrl, kafkaHostIp, csvName) -> 'string':
     """
@@ -28,6 +30,10 @@ def performanceTest(druidSqlUrl, kafkaHostIp, csvName) -> 'string':
         toAddRowsNumber = 500000
     elif csvName == 'data_1mln.csv':
         toAddRowsNumber = 1000000
+    elif csvName == 'data_2mln.csv':
+        toAddRowsNumber = 2000000
+    elif csvName == 'data_4mln.csv':
+        toAddRowsNumber = 4000000
     elif csvName == 'data_5mln.csv':
         toAddRowsNumber = 5000000
     else: 
@@ -66,7 +72,17 @@ for i in range(9):
     oneMillion.append(performanceTest(druidSqlUrl, kafkaIp, csvName4))
 print("List of times for 1 million data rows:\n", oneMillion)
 
+twoMillion = []
+for i in range(9):
+    twoMillion.append(performanceTest(druidSqlUrl, kafkaIp, csvName5))
+print("List of times for 2 million data rows:\n", twoMillion)
+
+fourMillion = []
+for i in range(9):
+    fourMillion.append(performanceTest(druidSqlUrl, kafkaIp, csvName6))
+print("List of times for 4 million data rows:\n", fourMillion)
+
 fiveMillion = []
 for i in range(1):
-    fiveMillion.append(performanceTest(druidSqlUrl, kafkaIp, csvName5))
+    fiveMillion.append(performanceTest(druidSqlUrl, kafkaIp, csvName7))
 print("List of times for 5 million thousands data rows:\n", fiveMillion)
